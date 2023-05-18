268c24dd61348673f9f886635a1cbfedff5d2e96
Xiao Wu <996232897@qq.com>
2023-05-18 09:39:18 +0800

    Add more options

--- tutorial/cpp/IVFPQ-GPU.cpp
+++ tutorial/cpp/IVFPQ-GPU.cpp
@@ -19,13 +19,15 @@
 using idx_t = faiss::idx_t;
 
 int main(int argc, char** argv) {
-    int d = 1024;      // dimension
+    int d = 1024;    // dimension
     int nb = 100000; // database size
     int nq = 10000;  // number of queries
     int nlist = 100; // number of lists for IVF indexing
-    int k = 100; // k nearest neighbors to be found
-    int m = 8; // number of subquantizers
-
+    int nprobe = 10; // number of lists to search for IVF indexing
+    int k = 100;     // k nearest neighbors to be found
+    int m = 8;       // number of subquantizers
+    int bitsPerCode = 8; 
+    bool usePrecomputedTables = false;
     namespace po = boost::program_options;
     po::options_description desc("Allowed options");
     desc.add_options()
@@ -34,8 +36,11 @@ int main(int argc, char** argv) {
     ("nb", po::value<int>(&nb)->default_value(100000),  "dimension")
     ("nq", po::value<int>(&nq)->default_value(10000), "number of queries")
     ("nlist", po::value<int>(&nlist)->default_value(100), "number of lists for IVF indexing")
+    ("nprobe", po::value<int>(&nprobe)->default_value(10), "number of lists to search for IVF indexing")
     ("k", po::value<int>(&k)->default_value(100),  "k nearest neighbors to be found")
     ("m", po::value<int>(&m)->default_value(8), "number of subquantizers")
+    ("bits", po::value<int>(&bitsPerCode)->default_value(8), "")
+    ("usePrecomputedTables, u", po::value<bool>(&usePrecomputedTables)->default_value(false), "")
   ;
 
     // Parse command line arguments
@@ -43,12 +48,8 @@ int main(int argc, char** argv) {
     po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
     po::notify(vm);
 
-    // Check if there are enough args or if --help is given
-    //if (vm.count ("help") || !vm.count ("input") || !vm.count ("output")) {
-    //    std::cerr << desc << "\n";
-    //    return 1;
-    //}
-    std::cout << "d" << d << "_nb" << nb << "_nq" << nq << "_nlist" << nlist << "_k" << k << "_m" << m << std::endl;
+    std::cout << "d" << d << "_nb" << nb << "_nq" << nq << "_nlist" << nlist << "_k" << k << "_m" << 
+     m << "_bits" << bitsPerCode << "_u" << usePrecomputedTables << std::endl;
 
     std::mt19937 rng;
     std::uniform_real_distribution<> distrib;
@@ -71,9 +72,10 @@ int main(int argc, char** argv) {
     faiss::gpu::StandardGpuResources res;
 
     faiss::gpu::GpuIndexIVFPQConfig config;
-    config.usePrecomputedTables = true;
-    faiss::gpu::GpuIndexFlatL2 quantizer(&res, d); // the other index
-    faiss::gpu::GpuIndexIVFPQ index(&res, &quantizer, d, nlist, m, 8);
+    config.usePrecomputedTables = true; // default = false
+
+    faiss::gpu::GpuIndexFlatL2 quantizer(&res, d); 
+    faiss::gpu::GpuIndexIVFPQ index(&res, &quantizer, d, nlist, m, bitsPerCode, faiss::METRIC_L2, config);
     std::cout << index.getPrecomputedCodes() << std::endl;
 
     index.train(nb, xb);
@@ -83,7 +85,7 @@ int main(int argc, char** argv) {
         idx_t* I = new idx_t[k * nq];
         float* D = new float[k * nq];
 
-        index.nprobe = 10;
+        index.nprobe = nprobe;
         index.search(nq, xq, k, D, I);
 
         delete[] I;
